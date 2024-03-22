bd24dba8bd181ca3e6452a575c54b9b89a9d3f09
Arturo Arreola <86033550+arturoarreola88@users.noreply.github.com>
2024-03-21 22:28:54 -0500

    new-login-script

--- code/login.js
+++ code/login.js
@@ -1,54 +1,49 @@
 function handleCredentialResponse(response) {
     console.log("Credential Response Received", response);
-
-    // You'll need to send this response back to your server to validate the token and create a session.
-    // For demonstration, we'll decode the JWT to extract the user's information
-    // Note: It's not secure to rely on this information without validating the token on the server side.
-
     const data = parseJwt(response.credential);
     console.log(data);
-
-    // Display confirmation message
     displayConfirmation(data.name);
-
-    // Hide the Google Sign-In button
     hideSignInButton();
 }
 
 function displayConfirmation(userName) {
     const confirmationMessageHTML = `<p>Welcome, ${userName}! You have successfully signed in.</p>`;
     const confirmationContainer = document.createElement("div");
+    confirmationContainer.style.position = "fixed";
+    confirmationContainer.style.top = "0";
+    confirmationContainer.style.right = "0";
+    confirmationContainer.style.backgroundColor = "#f8f9fa";
+    confirmationContainer.style.borderBottomLeftRadius = "5px";
+    confirmationContainer.style.padding = "10px";
     confirmationContainer.innerHTML = confirmationMessageHTML;
     document.body.appendChild(confirmationContainer);
 }
 
 function hideSignInButton() {
-    const signInButtonContainer = document.getElementById("signin-container");
+    const signInButtonContainer = document.getElementById("g_id_onload");
     if (signInButtonContainer) {
         signInButtonContainer.style.display = 'none';
     }
 }
 
-// Helper function to parse JWT
 function parseJwt(token) {
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
-
     return JSON.parse(jsonPayload);
 };
 
-// This setup is for demonstration. In a real application, the token should be sent to your server for validation
 window.onload = function () {
     google.accounts.id.initialize({
         client_id: "576961548022-g20uu1iqf1frsf20kbpq2u09osfe66g0.apps.googleusercontent.com",
-        callback: handleCredentialResponse
+        callback: handleCredentialResponse,
+        auto_select: false
     });
     google.accounts.id.renderButton(
-        document.getElementById("signin-container"), // Ensure this ID matches the container where you want the sign-in button to appear
-        { theme: "outline", size: "large" }  // Customization options
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" }
     );
-    google.accounts.id.prompt(); // Display the One Tap sign-in prompt
+    google.accounts.id.prompt();
 };
