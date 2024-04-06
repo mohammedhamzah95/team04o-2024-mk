91c37b995afa7aea94ed6914b00f505e55a28ec7
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-06 15:30:10 -0500

    implement user login and registration functions

--- code/backend/controllers/authController.js
+++ code/backend/controllers/authController.js
@@ -1,10 +1,11 @@
 
-const bcrypt = require('bycrypt')
+const bcrypt = require('bcrypt')
 const User = require('../models/users')
 const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
     try {
+
         const { name, email, password } = req.body
         const hashedPassword = await bcrypt.hash(password, 10)
         console.log(hashedPassword)
@@ -21,16 +22,25 @@ const register = async (req, res) => {
     }
 }
 
+
 const login = async (req, res) => {
     try {
-        // Code for user login
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { email } });
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, { expiresIn: '2d' });
+
+        res.status(200).json({ token, user: { id: user.id, name: user.name, email: user.email } });
     } catch (error) {
         res.status(500).json({ message: "Server error" });
     }
 }
 
-module.exports = {
-    register,
-    login
+module.exports={
+    register,login
 }
-		
