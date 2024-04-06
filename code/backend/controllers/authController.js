1cf123aeb396ff9701d01144be730ca006d6f378
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-06 15:27:43 -0500

    implement user registration

--- code/backend/controllers/authController.js
+++ code/backend/controllers/authController.js
@@ -5,7 +5,17 @@ const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
     try {
-        // Code for user registration
+        const { name, email, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+        console.log(hashedPassword)
+        await User.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        return res.json({message:"User Registered!"})
+
     } catch (error) {
         res.status(500).json({ message: "Server error" })
     }
