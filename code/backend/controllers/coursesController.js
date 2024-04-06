5487fc9a2aa9301d68db249f04cd4cd14555b0c2
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-06 15:35:14 -0500

    implement course retrieval

--- code/backend/controllers/coursesController.js
+++ code/backend/controllers/coursesController.js
@@ -5,10 +5,11 @@ const getCoursesAndInstructors = async (req, res) => {
 };
 
 const getAllCourses = async (req, res) => {
-    
-};
-
-module.exports = {
-    getAllCourses,
-    getCoursesAndInstructors
+    try {
+        const courses = await Course.findAll();
+        res.status(200).json({ courses });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        res.status(500).json({ message: "Server error" });
+    }
 };
