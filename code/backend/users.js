5c4e7ace62f7813dffde1ea4ba904a9ef190dafa
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-06 16:44:46 -0500

    create user model

--- code/backend/users.js
+++ code/backend/users.js
@@ -1,2 +1,17 @@
 const { sequelize } = require('../config/connection')
 const {DataTypes} = require('sequelize')
+
+const User = sequelize.define('User', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
