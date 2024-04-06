5875469b43cd1e74e3956b018fcf83212ec05128
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-06 15:43:46 -0500

    set up controller for instructor retrieval

---
+++ code/backend/controllers/instructorController.js
@@ -0,0 +1,15 @@
+const Review = require('../models/reviews');
+const Instructor = require('../models/instructor');
+
+const getInstructor = async (req, res) => {
+    
+};
+
+const getInstructors = async (req, res) => {
+    
+};
+
+module.exports = {
+    getInstructor,
+    getInstructors
+};
