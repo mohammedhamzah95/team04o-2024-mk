5437130f2ddccdca573c076d55b2ae4f8553da01
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-06 15:52:36 -0500

    base controller for review

---
+++ code/backend/controllers/reviewController.js
@@ -0,0 +1,15 @@
+const Review = require('../models/reviews');
+const User = require('../models/users');
+
+const createInstructorReview = async (req, res) => {
+    
+};
+
+const getInstructorReviews = async (req, res) => {
+    
+};
+
+module.exports = {
+    createInstructorReview,
+    getInstructorReviews
+};
