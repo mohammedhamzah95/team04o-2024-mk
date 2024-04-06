505eb48300afa4d274efca2682cf67e70dcc3cfe
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-06 15:32:47 -0500

    First commit

---
+++ code/backend/controllers/coursesController.js
@@ -0,0 +1,14 @@
+const Course = require('../models/courses');
+
+const getCoursesAndInstructors = async (req, res) => {
+    
+};
+
+const getAllCourses = async (req, res) => {
+    
+};
+
+module.exports = {
+    getAllCourses,
+    getCoursesAndInstructors
+};
