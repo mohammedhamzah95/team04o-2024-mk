2ba4459e33bd99916abdf8c6fdde00b710d5b3b2
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-08 02:37:51 -0500

    Declared identifier twice

--- code/backend/controllers/coursesController.js
+++ code/backend/controllers/coursesController.js
@@ -1,12 +1,9 @@
 const Course = require('../models/courses');
 
 const getCoursesAndInstructors = async (req, res) => {
-    
-};
-
-const getAllCourses = async (req, res) => {
     try {
         const { id } = req.params;
+
         const course = await Course.findOne({ where: { id } });
 
         if (!course) {
@@ -14,12 +11,14 @@ const getAllCourses = async (req, res) => {
         }
 
         const instructors = await course.getInstructors();
+
         res.status(200).json({ course, instructors });
     } catch (error) {
         console.error("Error fetching data:", error);
         res.status(500).json({ message: "Server error" });
     }
 };
+
 const getAllCourses = async (req, res) => {
     try {
        
