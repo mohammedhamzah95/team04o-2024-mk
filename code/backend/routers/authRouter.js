f104d3b9fac3494e70af8426815e8dd242799a4b
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-12 17:27:04 -0500

    add google auth route

--- code/backend/routers/authRouter.js
+++ code/backend/routers/authRouter.js
@@ -1,7 +1,8 @@
-const {register,login} =require('../controllers/authController')
-const {Router}=require('express')
-const router=Router()
-const verifyToken=require('../middleware/checkToken')
+const { register, login,authWithGoogle } = require('../controllers/authController')
+const { Router } = require('express')
+const router = Router()
+const verifyToken = require('../middleware/checkToken')
+const passport = require('passport')
 
 router.post('/register',register)
 router.post('/login',login)
@@ -9,4 +10,8 @@ router.get('/check-token', verifyToken, (req, res) => {
     res.status(200).json({ message: 'Token is valid', user: req.user });
 });
 
-module.exports=router
+router.post('/', authWithGoogle)
+
+
+
+module.exports = router
