d4e2c06a54c6e1ef7db3a0445aeeff6fafebfec2
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-12 17:18:37 -0500

    check for existing user before sign-in

--- code/backend/config/sync.js
+++ code/backend/config/sync.js
@@ -1,7 +1,3 @@
-const {sequelize} = require('./connection');
-
-const { User, Instructor, Review } = require('./associations');
-
 sequelize.sync()
   .then(() => {
     console.log('Database synchronized successfully.');
--- code/backend/controllers/authController.js
+++ code/backend/controllers/authController.js
@@ -6,7 +6,12 @@ const jwt = require('jsonwebtoken')
 const register = async (req, res) => {
     try {
 
-        const { name, email, password } = req.body
+        const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
         const hashedPassword = await bcrypt.hash(password, 10)
         console.log(hashedPassword)
         await User.create({
