401e8bc50264a1f3df8ad583e540aadaa9de5a0f
Mohammed Khan <mohammedhamzah1995@gmail.com>
2024-04-12 17:21:14 -0500

    add oauth function

--- code/backend/controllers/authController.js
+++ code/backend/controllers/authController.js
@@ -13,17 +13,18 @@ const register = async (req, res) => {
             return res.status(400).json({ message: "User already exists" });
         }
         const hashedPassword = await bcrypt.hash(password, 10)
-        console.log(hashedPassword)
+        
         await User.create({
             name,
             email,
             password: hashedPassword
-        })
+        });
 
-        return res.json({message:"User Registered!"})
+        return res.json({message:"User Registered!"});
 
     } catch (error) {
-        res.status(500).json({ message: "Server error" })
+        console.error("Error:", error);
+        return res.status(500).json({ message: "Server error" });
     }
 }
 
@@ -46,8 +47,33 @@ const login = async (req, res) => {
     }
 }
 
-module.exports={
-    register,login
+
+const authWithGoogle = async (req, res) => {
+    try {
+        const { googleId, email, name } = req.body;
+
+        console.log(req.body)
+        if (!googleId || !email || !name) {
+            return res.status(400).json({ message: "Missing required fields" });
+        }
+
+        let user = await User.findOne({ where: { googleId } });
+
+        if (!user) {
+            user = await User.create({ googleId, name, email });
+        }
+
+        const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY, { expiresIn: '2d' });
+
+        res.status(200).json({ token, user: { id: user.id, name: user.name, email: user.email } });
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
+
+module.exports = {
+    register, login, authWithGoogle
 }
 
 
